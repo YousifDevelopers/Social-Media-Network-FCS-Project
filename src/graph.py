a89cbdf0ea9e82ffe38e97dd7e84222451de05a6
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-07-24 23:11:48 +0300

    Update Graph class with remove_edge and remove_node methods

--- src/graph.py
+++ src/graph.py
@@ -10,3 +10,15 @@ class Graph:
         if node1 in self.adjacency_list and node2 in self.adjacency_list:
             self.adjacency_list[node1].append(node2)
             self.adjacency_list[node2].append(node1)
+    def remove_edge(self, node1, node2):
+        if node1 in self.adjacency_list and node2 in self.adjacency_list:
+            if node2 in self.adjacency_list[node1]:
+                self.adjacency_list[node1].remove(node2)
+            if node1 in self.adjacency_list[node2]:
+                self.adjacency_list[node2].remove(node1)
+    
+    def remove_node(self, node):
+        if node in self.adjacency_list:
+            for neighbour in self.adjacency_list[node]:
+                self.adjacency_list[neighbour].remove(node)
+            del self.adjacency_list[node]
