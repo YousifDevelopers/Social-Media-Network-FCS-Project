8aeafd9b484f08638c314e6d531f0a286206cccd
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-07-27 15:59:59 +0300

    Modify test network file to test two methods  user_follow_network and user_club_network

--- test/test_network.py
+++ test/test_network.py
@@ -3,6 +3,8 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from src.network import SocialNetwork
-social_network = SocialNetwork()
+follow_user_network = SocialNetwork()
+follow_user_network.user_follow_network()
 
-social_network.user_follow_network()
+clus_user_network = SocialNetwork()
+clus_user_network.user_club_network()
